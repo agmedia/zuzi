39eb5a2d5a66afb772ac460fb9dd0065a2ce8f4d
tomashina <tomislav@agmedia.hr>
2023-05-24 11:57:41 +0200

    fixes

--- resources/views/front/catalog/category/product.blade.php
+++ resources/views/front/catalog/category/product.blade.php
@@ -1,6 +1,6 @@
-<div class="article 0" >
+<div class="article pb-1" >
 
-<div class="card product-card d-flex align-items-stretch shadow ">
+<div class="card product-card d-flex align-items-stretch shadow pb-1">
     @if ($product->main_price > $product->main_special)
         <span class="badge rounded-pill bg-primary mt-1 ms-1 badge-shadow">-{{ number_format(floatval(\App\Helpers\Helper::calculateDiscount($product->price, $product->special())), 0) }}%</span>
 
