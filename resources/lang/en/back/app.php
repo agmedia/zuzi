c48cf69cc9955cf77139abff4306b2db548376ec
Filip Jankoski <filip@agmedia.hr>
2023-06-09 11:12:30 +0200

    _added: lang file for corvus.

---
+++ resources/lang/en/back/app.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'payments'=> [
+        'title' => 'Payment methods',
+        'edit_title' => 'Edit',
+        'input_title' => 'Title',
+        'status_title' => 'Status',
+        'code' => 'Code',
+        'sort_order' => 'Sort order',
+        'list' => 'List',
+        'save' => 'Save',
+        'cancel' => 'Cancel',
+        'empty_list' => 'No payment gateways...',
+        'cod' => 'COD (Cash on Delivery)',
+        'bank' => 'Bank transfer',
+        'wspay' => 'WSPay',
+        'payway' => 'T-Com Payway',
+        'corvus' => 'Corvus Pay',
+        'min_order_amount' => 'Minimum order amount',
+        'fee_amount' => 'Fee amount',
+        'geo_zone' => 'Geo zone',
+        'geo_zone_label' => '(Geo zone to which payment applies.)',
+        'short_desc' => 'Short description',
+        'short_desc_label' => '(Displayed when selecting payment.)',
+        'long_desc' => 'Long description',
+        'long_desc_label' => '(If required. Displayed if payment is selected during purchase.)',
+        'chars' => 'chars',
+        'select_geo' => 'Select geo zone',
+    ]
+];
