440862b7baf1686e826c791d904489aaf749124b
tomashina <tomislav@agmedia.hr>
2023-06-12 10:28:54 +0200

    corvus front fix

--- resources/views/front/checkout/payment/corvus.blade.php
+++ resources/views/front/checkout/payment/corvus.blade.php
@@ -1,30 +1,4 @@
-<h4 class="text-secondary my-4 mt-4">{{ __('front/checkout.personal_info') }}</h4>
-<table class="table table-striped mb-5">
-    <tbody>
-    <tr>
-        <th scope="row">{{ __('front/checkout.name') }}</th>
-        <td>{{ $data['firstname'] }}</td>
-
-    </tr>
-    <tr>
-        <th scope="row">{{ __('front/checkout.surname') }}</th>
-        <td>{{ $data['lastname'] }}</td>
-
-    </tr>
-    <tr>
-        <th scope="row">{{ __('front/checkout.mobile_number') }}</th>
-        <td>{{ $data['telephone'] }}</td>
-    </tr>
-    <tr>
-        <th scope="row">{{ __('front/checkout.email_address') }}</th>
-        <td>{{ $data['email'] }}</td>
-    </tr>
-    </tbody>
-</table>
-
-
 <form name="pay" class="w-100" action="{{ $data['action'] }}" method="POST">
-
     <input type="hidden" name="amount" value="{{ $data['total'] }}">
     <input id="cart" name="cart" value="Web shop kupnja {{ $data['order_id'] }}" type="hidden"/>
     <input id="currency" name="currency" value="{{ $data['currency'] }}" type="hidden"/>
@@ -41,10 +15,8 @@
     <input type="hidden" name="version" value="1.3">
 
     <div class="d-flex mt-3">
-        <div class="w-50 pe-3"><a class="btn btn-secondary d-block w-100" href="{{ route('index') }}"><i class="ci-arrow-left  me-1"></i><span class="d-none d-sm-inline">{{ __('front/checkout.backbtnpayment') }}</span><span class="d-inline d-sm-none">{{ __('front/checkout.backbtn') }}</span></a></div>
-        <div class="w-50 ps-2">
-            <button class="btn btn-primary d-block w-100" type="submit"><span class="d-none d-sm-inline">{{ __('front/checkout.finishorder') }}</span><span class="d-inline d-sm-none">{{ __('front/checkout.finishshoping') }}</span><i class="ci-arrow-right ms-1"></i></button>
-        </div>
+        <div class="w-50 pe-3"><a class="btn btn-secondary d-block w-100" href="{{ route('naplata') }}"><i class="ci-arrow-left mt-sm-0 me-1"></i><span class="d-none d-sm-inline">Povratak na plaćanje</span><span class="d-inline d-sm-none">Povratak</span></a></div>
+        <div class="w-50 ps-2"><button class="btn btn-primary d-block w-100" type="submit"><span class="d-none d-sm-inline">Završite narudžbu</span><span class="d-inline d-sm-none">Završi kupnju</span><i class="ci-arrow-right mt-sm-0 ms-1"></i></button></div>
     </div>
     <div class="clearfix"></div>
 </form>
