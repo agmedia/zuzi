d3d2f576dfe09f906efd3f1ff1c3f3d4cc2f21fa
Filip Jankoski <filip@agmedia.hr>
2023-06-12 10:24:16 +0200

    _added: corvus lang.

---
+++ resources/lang/en/front/checkout.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'main_title' => 'Confirm and pay',
+
+    'entire_rental_unit' => 'Entire rental unit',
+    'price_details' => 'Price details',
+    'your_reservation' => 'Your reservation',
+    'dates' => 'Dates',
+    'Guests' => 'Guests',
+    'guests' => 'guests',
+    'options' => 'Fixed Fees',
+    'additional_options' => 'Additional Options',
+    'personal_info' => 'Personal Info',
+    'name' => 'Name',
+    'surname' => 'Surname',
+    'mobile_number' => 'Mobile number',
+    'email_address' => 'Email Address',
+    'pay_with' => 'Pay with',
+    'additional_comments' => 'Additional Comments',
+    'type_comments' => 'Type Comments...',
+    'agree' => 'By selecting the button below, I agree to the <a href="en/info/terms-and-conditions" class="alert-link">updated Terms of Service, Payments Terms of Service, and I acknowledge the Privacy Policy.</a>',
+    'confirm_and_pay' => 'Confirm and Pay',
+    'subtotal' => 'Subtotal',
+    'tax' => 'Tax <small class="text-muted">(PDV 25%)</small>',
+    'total' => 'Total',
+    'regulardays' => 'Regular days',
+    'regulardaysspecial' => 'Regular days on Special price',
+    'weekends' => 'Weekends',
+    'weekendsspecial' => 'Weekends on Special price',
+    'preview' => 'Checkout Preview',
+    'special' => 'Special Checkout Preview',
+    'backbtn' => 'Back',
+    'backbtnpayment' => 'Back to payment',
+    'finishorder' => 'Finish order',
+    'finishshoping' => 'Finish payment',
+    'register' => 'Register as a new user!',
+    //
+    'select_source' => 'Select the source of your reservation...',
+    'dates_error' => 'Please select the dates of your reservation.',
+    'source_error' => 'Please select the source of your reservation.',
+    'arbitrary' => [
+        'title' => 'Please select the dates and source of Your reservation.',
+        'title2' => 'Molimo odaberite Vaš apartman.',
+        'price' => 'Purpose and amount info',
+        'select_scope' => 'Select purpose of payment...',
+        'amount' => 'Enter amount',
+        'btn_next' => 'Next'
+    ],
+];
