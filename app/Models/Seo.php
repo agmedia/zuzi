8b6d38ba6bb6d85678f97327d6f7bf35b5c7da5f
tomashina <tomislav@agmedia.hr>
2023-06-13 11:15:11 +0200

    fix biblos

--- app/Models/Seo.php
+++ app/Models/Seo.php
@@ -24,7 +24,7 @@ class Seo
     {
         return [
             'title'       => $product->name . ' knjige ' . (isset($product->author->title) ? $product->author->title : ''),
-            'description' => 'Knjiga ' . $product->name . ' izdavača ' . (isset($product->author->title) ? $product->author->title : '') . ' godine izdanja ' . ($product->year ?: '') . ' i mjesta izdavanja ' . ($product->origin ?: '') . ' u Antikvarijatu Biblos.'
+            'description' => 'Knjiga ' . $product->name . ' izdavača ' . (isset($product->author->title) ? $product->author->title : '') . ' godine izdanja ' . ($product->year ?: '') . ' i mjesta izdavanja ' . ($product->origin ?: '') . ' u Zuzi Shop-u.'
         ];
     }
 
@@ -34,7 +34,7 @@ class Seo
      */
     public static function getAuthorData(Author $author, Category $cat = null, Category $subcat = null): array
     {
-        $title = $author->title . ' knjige - Antikvarijat Biblos';
+        $title = $author->title . ' knjige - Zuzi Shop';
         $description = 'Knjige autora ' . $author->title . ' danas su jako popularne u svijetu. Bogati izbor knjiga autora ' . $author->title . ' uz brzu dostavu i sigurnu kupovinu.';
 
         // Check if there is meta title or description and set vars.
@@ -60,7 +60,7 @@ class Seo
      */
     public static function getPublisherData(Publisher $publisher, Category $cat = null, Category $subcat = null): array
     {
-        $title = $publisher->title . ' knjige - Antikvarijat Biblos';
+        $title = $publisher->title . ' knjige - Zuzi Shop';
         $description = 'Ponuda knjiga nakladnika ' . $publisher->title . '. Knjige iz antikvarijata, naklade ' . $publisher->title . ' mogu biti u vašem domu uz brzu dostavu.';
 
         // Check if there is meta title or description and set vars.
