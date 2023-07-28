9874b89e2092f31a54eb1908a675f85f5a7aa704
Filip Jankoski <filip@agmedia.hr>
2023-07-28 09:32:52 +0200

    _fix: Widget price view.

--- resources/views/front/catalog/category/product.blade.php
+++ resources/views/front/catalog/category/product.blade.php
@@ -1,34 +1,34 @@
 <div class="article pb-1" >
 
-<div class="card product-card d-flex align-items-stretch shadow pb-1">
-    @if ($product->main_price > $product->main_special)
-        <span class="badge rounded-pill bg-primary mt-1 ms-1 badge-shadow">-{{ number_format(floatval(\App\Helpers\Helper::calculateDiscount($product->price, $product->special())), 0) }}%</span>
+    <div class="card product-card d-flex align-items-stretch shadow pb-1">
+        @if ($product->main_price > $product->main_special)
+            <span class="badge rounded-pill bg-primary mt-1 ms-1 badge-shadow">-{{ number_format(floatval(\App\Helpers\Helper::calculateDiscount($product->price, $product->special())), 0) }}%</span>
 
 
-    @endif
+        @endif
 
 
         <a class="card-img-top d-block overflow-hidden" href="{{ url($product->url) }}">
-        <img  src="{{ $product->thumb }}" width="250" height="300" alt="{{ $product->name }}">
-            </a>
-                <div class="card-body pt-2" style="min-height: 136px;">
+            <img  src="{{ $product->thumb }}" width="250" height="300" alt="{{ $product->name }}">
+        </a>
+        <div class="card-body pt-2" style="min-height: 136px;">
 
-                             <h3 class="product-title pt-2 fs-sm"><a href="{{ url($product->url) }}">{{ $product->name }}</a></h3>
+            <h3 class="product-title pt-2 fs-sm"><a href="{{ url($product->url) }}">{{ $product->name }}</a></h3>
 
-                            @if ($product->main_price > $product->main_special)
-                                <div class="product-price"><small><span class="text-muted">NC 30 dana: {{ $product->main_special_text }}  @if($product->secondary_price_text) {{ $product->secondary_special_text }} @endif</span></small></div>
-                        <div class="product-price"><span class="text-primary">{{ $product->main_price_text }}  @if($product->secondary_price_text) <small class="text-muted">{{ $product->secondary_price_text }}</small> @endif</span></div>
-                            @else
-                                <div class="product-price"><span class="text-primary">{{ $product->main_price_text }}  @if($product->secondary_price_text) <small class="text-muted">{{ $product->secondary_price_text }} </small>@endif</span></div>
-                            @endif
+            @if ($product->main_price > $product->main_special)
+                <div class="product-price"><small><span class="text-muted">NC 30 dana: {{ $product->main_price_text }}  @if($product->secondary_price_text) {{ $product->secondary_price_text }} @endif</span></small></div>
+                <div class="product-price"><span class="text-primary">{{ $product->main_special_text }}  @if($product->secondary_special_text) <small class="text-muted">{{ $product->secondary_special_text }}</small> @endif</span></div>
+            @else
+                <div class="product-price"><span class="text-primary">{{ $product->main_price_text }}  @if($product->secondary_price_text) <small class="text-muted">{{ $product->secondary_price_text }} </small>@endif</span></div>
+            @endif
 
 
-            </div>
+        </div>
 
         <div class="product-floating-btn">
             <add-to-cart-btn-simple id="{{ $product->id }}"></add-to-cart-btn-simple>
         </div>
-</div>
+    </div>
 
 </div>
 
