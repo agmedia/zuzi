e753011965d7b904fdd7e690a4847321886897bc
tomashina <tomislav@agmedia.hr>
2024-09-13 10:50:03 +0200

    laguna import xml fix

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -107,7 +107,7 @@ class DashboardController extends Controller
                     'publisher_id'     => $publisher ?: config('settings.unknown_publisher'),
                     'action_id'        => 0,
                     'name'             => $item->Naziv,
-                    'sku'              => 'lg'.$item->Šifra,
+                    'sku'             => $item->bar_kod,
                     'ean'              => $item->bar_kod,
                     'description'      => '<p class="text-primary">Rok dostave 20 radnih dana!</p><p>' . str_replace('\n', '<br>', $item->Opis) . '</p>',
                     'slug'             => Helper::resolveSlug($data),
