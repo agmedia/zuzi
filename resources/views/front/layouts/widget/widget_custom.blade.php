4c430b039a255ece2e3b8f0035c2b3fd380d9296
tomashina <tomislav@agmedia.hr>
2026-04-15 09:08:07 +0200

    remove rošendan

--- resources/views/front/layouts/widget/widget_custom.blade.php
+++ resources/views/front/layouts/widget/widget_custom.blade.php
@@ -29,7 +29,7 @@
                             <p class="text-dark  ">{{ $widget['subtitle'] }}</p>
                             <div class="d-flex flex-wrap justify-content-center justify-content-xl-start"><a class="btn btn-primary btn-shadow me-2 mb-2 slider-focus-btn" href="{{ url($widget['url']) }}" role="button">
 
-                                         Pogledajte akcije
+                                         Opširnije
 
                                      <i class="ci-arrow-right ms-2 me-n1"></i></a></div>
                         </div>
