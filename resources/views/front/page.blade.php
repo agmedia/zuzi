b8cf2a81d754eadb1e10cefba20c10420fb9030a
tomashina <tomislav@agmedia.hr>
2026-05-01 08:00:19 +0200

    sales

--- resources/views/front/page.blade.php
+++ resources/views/front/page.blade.php
@@ -640,7 +640,7 @@
                   </div>
                   <small class="d-block mb-0 home-promo-copy">
                       <strong><a href="https://www.zuzi.hr/blog/antikvarijat-i-knjizara-zuzi-shop-najbolji-samostalni-antikvarijat-i-knjizara-u-2023-godini"><u>Broj 1 online knjižara i antikvarijat u Hrvatskoj</u></a></strong> s više od
-                      <strong>84.000 artikala</strong>. BOX NOW dostava za samo <strong>0,99 €</strong>.
+                      <strong>84.000 artikala</strong>. 👉 🎁 Svibanj = <strong>BESPLATNA dostava uz BOXNOW</strong>.
                   </small>
               </div>
           </div>
